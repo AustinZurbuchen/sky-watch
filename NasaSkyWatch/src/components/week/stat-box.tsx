c1d4b8e4f9ffc0bff4d2e40abb100a822acae4b6
AustinZurbuchen <azzbuch129@gmail.com>
2026-05-28 14:27:19 -0700

    remove unused import

--- NasaSkyWatch/src/components/week/stat-box.tsx
+++ NasaSkyWatch/src/components/week/stat-box.tsx
@@ -1,4 +1,4 @@
-import { ColorValue, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { ThemedText } from "../theme/themed-text";
 import { ThemedView } from "../theme/themed-view";
 import { useTheme } from '@/hooks/use-theme';
