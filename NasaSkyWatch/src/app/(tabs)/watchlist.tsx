72b91cf1b728da16e87fb5f19383e015651e3319
AustinZurbuchen <azzbuch129@gmail.com>
2026-06-13 14:03:53 -0700

    fix ui for watchlist

--- NasaSkyWatch/src/app/(tabs)/watchlist.tsx
+++ NasaSkyWatch/src/app/(tabs)/watchlist.tsx
@@ -8,18 +8,20 @@ import { AsteroidFlyby } from '@/types';
 import { AsteroidCard } from '@/components/asteroid/AsteroidCard';
 import { EmptyState } from '@/components/watchlist/emptyState';
 import { useWatchlistStore } from '@/store/watchlistStore';
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import { router } from 'expo-router';
 
 const ITEM_HEIGHT = 100;
 const ITEM_SEPARATOR = 10;
 
 export default function WatchlistScreen() {
+  const listRef = useRef<FlatList<AsteroidFlyby>>(null);
   const savedAsteroids = useWatchlistStore((state) => state.savedAsteroids)
   const isEmpty = savedAsteroids.length === 0;
   
   const handleCardPress = useCallback((asteroid: AsteroidFlyby) => {
     console.log(asteroid);
+    console.log(savedAsteroids);
     router.push(`/asteroid/${asteroid.id}`);
   }, []);
 
@@ -40,17 +42,19 @@ export default function WatchlistScreen() {
     <ThemedView style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
         <ThemedTitle title="Watchlist" subtitle="Saved asteroids" />
-        {isEmpty ? <EmptyState /> : (
-          <FlatList<AsteroidFlyby>
-          data={savedAsteroids}
-          keyExtractor={(item) => item.id}
-          renderItem={renderItem}
-          getItemLayout={getItemLayout}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={styles.listContent}
-          ItemSeparatorComponent={() => <View style={{ height: ITEM_SEPARATOR }} />}
-        />
-        )}
+        <View style={styles.asteroidContainer}>
+          {isEmpty ? <EmptyState /> : (
+            <FlatList<AsteroidFlyby>
+              ref={listRef}
+              data={savedAsteroids}
+              showsVerticalScrollIndicator={false}
+              keyExtractor={(item) => item.id}
+              renderItem={renderItem}
+              getItemLayout={getItemLayout}
+              ItemSeparatorComponent={() => <View style={{ height: ITEM_SEPARATOR }} />}
+            />
+          )}
+        </View>
       </SafeAreaView>
     </ThemedView>
   )
@@ -61,6 +65,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     flexDirection: 'row',
+  },
+  asteroidContainer: {
+    height: '93%',
+    width: '100%',
   },
   listContent: {
     paddingBottom: 100,
